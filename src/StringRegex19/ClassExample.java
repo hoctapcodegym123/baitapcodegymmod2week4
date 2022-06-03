621f879c333762cd36814bdf403028c48ecc9930
hoanghai1310 <hoanghai.131096@gmail.com>
2022-06-03 09:51:29 +0700

    examples1

--- src/StringRegex19/ClassExample.java
+++ src/StringRegex19/ClassExample.java
@@ -1,2 +1,16 @@
-package StringRegex19;public class ClassExample {
+package StringRegex19;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ClassExample {
+
+    public final static String REGEX_CLASS_NAME = "^[CAP]\\d{4}[GHIKLM]";
+
+    public static void main(String[] args) {
+        String name = "C1234G";
+        System.out.println(name.matches(REGEX_CLASS_NAME));
+    }
 }
+
+//done
--- src/StringRegex19/TelephoneExample.java
+++ src/StringRegex19/TelephoneExample.java
@@ -1,4 +1,14 @@
 package StringRegex19;
 
-public class ClassExampleTest {
+public class TelephoneExample {
+    public final static String REGEX_TEL = "\\(\\d{2}\\)\\-\\(0\\d{9}\\)";
+
+
+    public static void main(String[] args) {
+        String telephone = "(34)-(0123456789)";
+        System.out.println(telephone.matches(REGEX_TEL));
+    }
+
 }
+
+//done
